962dbc84b0f0844c1d7d7f76744b0959ece1327e
tk-yoshimura <yoshimura8192@gmail.com>
2023-10-01 00:11:44 +0900

    fix chisquared

--- DoubleDoubleDistribution/ChiSquaredDistribution.cs
+++ DoubleDoubleDistribution/ChiSquaredDistribution.cs
@@ -7,15 +7,15 @@ namespace DoubleDoubleDistribution {
 
         public int K { get; }
 
-        private readonly ddouble pdf_norm, cdf_norm;
+        private readonly ddouble c, pdf_norm;
 
         public ChiSquaredDistribution(int k) {
             ValidateShape(k, k => k > 0);
 
             this.K = k;
 
-            this.cdf_norm = 1d / Gamma(k * 0.5d);
-            this.pdf_norm = Pow2(k * -0.5d) * cdf_norm;
+            this.c = K * 0.5d - 1d;
+            this.pdf_norm = k * 0.5d + LogGamma(k * 0.5d) * LbE;
         }
 
         public override ddouble PDF(ddouble x) {
@@ -23,15 +23,13 @@ namespace DoubleDoubleDistribution {
                 return K <= 1 ? PositiveInfinity : (K <= 2 ? Point5 : Zero);
             }
 
-            ddouble pdf = pdf_norm * Exp((K * 0.5d - 1d) * Log(x) - Ldexp(x, -1));
+            ddouble pdf = Pow2(c * Log2(x) - Ldexp(x, -1) * LbE - pdf_norm);
 
             return pdf;
         }
 
         public override ddouble CDF(ddouble x) {
-            ddouble cdf = cdf_norm * LowerIncompleteGamma(K * 0.5d, Ldexp(x, -1));
-
-            cdf = IsFinite(cdf) ? cdf : (x > One ? One : Zero);
+            ddouble cdf = LowerIncompleteGammaRegularized(K * 0.5d, Ldexp(x, -1));
 
             return cdf;
         }
