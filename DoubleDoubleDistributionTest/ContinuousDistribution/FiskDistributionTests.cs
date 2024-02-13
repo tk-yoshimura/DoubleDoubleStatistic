a831da857f62a1065d1503513aba8869d8a1b3e0
tk-yoshimura <yoshimura8192@gmail.com>
2024-02-13 23:31:03 +0900

    add: fisk

---
+++ DoubleDoubleDistributionTest/ContinuousDistribution/FiskDistributionTests.cs
@@ -0,0 +1,164 @@
+﻿using DoubleDouble;
+using DoubleDoubleDistribution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DoubleDoubleDistributionTest.ContinuousDistribution {
+    [TestClass()]
+    public class FiskDistributionTests {
+        readonly FiskDistribution dist_c1 = new(c: 1);
+        readonly FiskDistribution dist_c2 = new(c: 2);
+        readonly FiskDistribution dist_c3 = new(c: 3);
+
+        FiskDistribution[] Dists => new[]{
+            dist_c1,
+            dist_c2,
+            dist_c3,
+        };
+
+        [TestMethod()]
+        public void InfoTest() {
+            foreach (FiskDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                Console.WriteLine($"Support={dist.Support}");
+                Console.WriteLine($"C={dist.C}");
+                Console.WriteLine($"Mean={dist.Mean}");
+                Console.WriteLine($"Median={dist.Median}");
+                Console.WriteLine($"Mode={dist.Mode}");
+                Console.WriteLine($"Variance={dist.Variance}");
+                Console.WriteLine($"Skewness={dist.Skewness}");
+                Console.WriteLine($"Kurtosis={dist.Kurtosis}");
+                /* TODO: implement entropy */
+                //Console.WriteLine($"Entropy={dist.Entropy}");
+            }
+        }
+
+        [TestMethod()]
+        public void PDFTest() {
+            foreach (FiskDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                for (ddouble x = 0; x <= 4; x += 0.125) {
+                    ddouble pdf = dist.PDF(x);
+
+                    Console.WriteLine($"pdf({x})={pdf}");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void CDFLowerTest() {
+            foreach (FiskDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                for (ddouble x = 0; x <= 4; x += 0.125) {
+                    ddouble cdf = dist.CDF(x, Interval.Lower);
+
+                    Console.WriteLine($"cdf({x})={cdf}");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void CDFUpperTest() {
+            foreach (FiskDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                for (ddouble x = 0; x <= 4; x += 0.125) {
+                    ddouble cdf = dist.CDF(x, Interval.Lower);
+                    ddouble ccdf = dist.CDF(x, Interval.Upper);
+
+                    Console.WriteLine($"ccdf({x})={ccdf}");
+
+                    Assert.IsTrue(ddouble.Abs(cdf + ccdf - 1) < 1e-28);
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void QuantileLowerTest() {
+            foreach (FiskDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                for (int i = 0; i <= 10; i++) {
+                    ddouble p = (ddouble)i / 10;
+                    ddouble x = dist.Quantile(p, Interval.Lower);
+                    ddouble cdf = dist.CDF(x, Interval.Lower);
+
+                    Console.WriteLine($"quantile({p})={x}, cdf({x})={cdf}");
+
+                    if (ddouble.IsFinite(x)) {
+                        Assert.IsTrue(ddouble.Abs(p - cdf) < 1e-28);
+                    }
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void QuantileUpperTest() {
+            foreach (FiskDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                for (int i = 0; i <= 10; i++) {
+                    ddouble p = (ddouble)i / 10;
+                    ddouble x = dist.Quantile(p, Interval.Upper);
+                    ddouble ccdf = dist.CDF(x, Interval.Upper);
+
+                    Console.WriteLine($"cquantile({p})={x}, ccdf({x})={ccdf}");
+
+                    if (ddouble.IsFinite(x)) {
+                        Assert.IsTrue(ddouble.Abs(p - ccdf) < 1e-28);
+                    }
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void PDFExpectedTest() {
+            ddouble[] expected_dist_c1 = [
+            ];
+            ddouble[] expected_dist_c2 = [
+            ];
+            ddouble[] expected_dist_c3 = [
+            ];
+
+            foreach ((FiskDistribution dist, ddouble[] expecteds) in new[]{
+                (dist_c1, expected_dist_c1), 
+                (dist_c2, expected_dist_c2), 
+                (dist_c3, expected_dist_c3),
+            }) {
+                for ((ddouble x, int i) = (0, 0); i < expecteds.Length; x += 0.5, i++) {
+                    ddouble expected = expecteds[i];
+                    ddouble actual = dist.PDF(x);
+
+                    Console.WriteLine($"{dist} pdf({x})");
+                    Console.WriteLine(expected);
+                    Console.WriteLine(actual);
+
+                    Assert.IsTrue(ddouble.Abs(expected - actual) / expected < 1e-30, $"{dist} pdf({x})\n{expected}\n{actual}");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void CDFExpectedTest() {
+            ddouble[] expected_dist_c1 = [
+            ];
+            ddouble[] expected_dist_c2 = [
+            ];
+            ddouble[] expected_dist_c3 = [
+            ];
+
+            foreach ((FiskDistribution dist, ddouble[] expecteds) in new[]{
+                (dist_c1, expected_dist_c1), 
+                (dist_c2, expected_dist_c2), 
+                (dist_c3, expected_dist_c3),
+            }) {
+                for ((ddouble x, int i) = (0, 0); i < expecteds.Length; x += 0.5, i++) {
+                    ddouble expected = expecteds[i];
+                    ddouble actual = dist.CDF(x);
+
+                    Console.WriteLine($"{dist} cdf({x})");
+                    Console.WriteLine(expected);
+                    Console.WriteLine(actual);
+
+                    Assert.IsTrue(ddouble.Abs(expected - actual) / expected < 1e-30, $"{dist} cdf({x})\n{expected}\n{actual}");
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -24,7 +24,7 @@ T: TODO Test
 |            | gumbel             | T     | T     | T        | T     | N          | N      |            |
 |            | gompertz           | T     | T     | T        | T     | N          | N      |            |
 |            | fisher z           | T     | T     | T        | T     | N          | N      |            |
-|            | fisk               | N     | N     | N        | N     | N          | N      | C          |
+|            | fisk               | T     | T     | T        | T     | N          | N      |            |
 |            | frechet            | N     | N     | N        | N     | N          | N      | C          |
 |            | half normal        | T     | T     | T        | T     | N          | N      |            |
 |            | hyperbolic secant  | N     | N     | N        | N     | N          | N      | C          |
@@ -60,7 +60,7 @@ T: TODO Test
 |            | weibull            | T     | T     | T        | T     | N          | N      |            |
 |            | wigner semicircle  | C     | C     | C        | T     | N          | N      |            |
 |            | zold               | N     | N     | N        | N     | N          | N      | C          |
-|            | ***coverage***     | 32/52 | 32/52 | 31/52    | 32/52 | 0/52       | 0/52   |            |
+|            | ***coverage***     | 33/52 | 33/52 | 32/52    | 33/52 | 0/52       | 0/52   |            |
 | stable     | cauchy             | C     | C     | C        | T     | N          | N      |            |
 |            | delta              | C     | C     | C        | T     | -          | N      |            |
 |            | holtsmark          | N     | N     | N        | N     | N          | N      | A          |
