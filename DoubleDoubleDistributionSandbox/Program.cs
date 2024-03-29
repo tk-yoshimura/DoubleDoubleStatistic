b7910504be2752ae457cc02d8495c963526edf42
tk-yoshimura <yoshimura8192@gmail.com>
2024-03-30 01:30:48 +0900

    add: sandbox

---
+++ DoubleDoubleDistributionSandbox/Program.cs
@@ -0,0 +1,19 @@
+﻿using DoubleDouble;
+using DoubleDoubleDistribution;
+
+namespace DoubleDoubleDistributionSandbox {
+    internal class Program {
+        static void Main() {
+            LandauDistribution dist = new(mu: 0, c: ddouble.PI / 2);
+
+            for (ddouble x = -4; x <= 16; x += 1 / 256d) {
+                ddouble y = dist.PDF(x);
+
+                Console.WriteLine($"{x},{y}");
+            }
+
+            Console.WriteLine("END");
+            Console.Read();
+        }
+    }
+}
