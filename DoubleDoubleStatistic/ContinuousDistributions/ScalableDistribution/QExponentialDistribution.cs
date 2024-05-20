823cc8cbd7442a6e52b680e23bd5e0e3765e03ed
tk-yoshimura <yoshimura8192@gmail.com>
2024-05-20 11:47:09 +0900

    add: q exp

---
+++ DoubleDoubleStatistic/ContinuousDistributions/ScalableDistribution/QExponentialDistribution.cs
@@ -0,0 +1,245 @@
+﻿using DoubleDouble;
+using DoubleDoubleStatistic.InternalUtils;
+using DoubleDoubleStatistic.Misc;
+using DoubleDoubleStatistic.Optimizer;
+using DoubleDoubleStatistic.RandomGeneration;
+using DoubleDoubleStatistic.SampleStatistic;
+using DoubleDoubleStatistic.Utils;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Numerics;
+using static DoubleDouble.ddouble;
+
+namespace DoubleDoubleStatistic.ContinuousDistributions {
+    [DebuggerDisplay("{ToString(),nq}")]
+    public class QExponentialDistribution : ScalableDistribution<QExponentialDistribution>,
+        IMultiplyOperators<QExponentialDistribution, ddouble, QExponentialDistribution>,
+        IDivisionOperators<QExponentialDistribution, ddouble, QExponentialDistribution>,
+        IFittableContinuousDistribution<QExponentialDistribution> {
+
+        public ddouble Q { get; }
+        public ddouble Theta { get; }
+
+        private readonly ddouble pdf_power, cdf_power, quantile_power, pdf_norm, theta_inv, q12;
+
+        public QExponentialDistribution(ddouble q) : this(q, theta: 1d) { }
+
+        public QExponentialDistribution(ddouble q, ddouble theta) {
+            ParamAssert.ValidateShape(nameof(q), q >= 0 && q < 2);
+            ParamAssert.ValidateScale(nameof(theta), ParamAssert.IsFinitePositive(theta));
+
+            Q = q;
+            Theta = theta;
+
+            theta_inv = 1d / theta;
+
+            pdf_norm = (2d - Q) * theta_inv;
+            pdf_power = 1d / (1d - q);
+            cdf_power = (Q - 2d) / (Q - 1d);
+            quantile_power = (Q - 1d) / (Q - 2d);
+            q12 = (Q - 1d) * (Q - 2d);
+        }
+
+        public override ddouble PDF(ddouble x) {
+            ddouble u = x * theta_inv;
+
+            if (IsNaN(u)) {
+                return NaN;
+            }
+
+            if (IsNegative(u) || IsInfinity(u)) {
+                return 0d;
+            }
+
+            if (IsFinite(pdf_power)) {
+                ddouble pdf = pdf_norm * Pow(Max(0d, 1d + (Q - 1d) * u), pdf_power);
+
+                if (IsNaN(pdf)) {
+                    return 0d;
+                }
+
+                return pdf;
+            }
+            else {
+                ddouble pdf = pdf_norm * Exp(-u);
+
+                return pdf;
+            }
+        }
+
+        public override ddouble CDF(ddouble x, Interval interval = Interval.Lower) {
+            ddouble u = x * theta_inv;
+
+            if (IsNaN(u)) {
+                return NaN;
+            }
+
+            if (interval == Interval.Lower) {
+                if (IsNegative(u)) {
+                    return 0d;
+                }
+                if (IsPositiveInfinity(u)) {
+                    return 1d;
+                }
+
+                if (IsFinite(cdf_power)) {
+                    ddouble cdf = 1d - Pow(Max(0d, 1d + (Q - 1d) * u), cdf_power);
+
+                    cdf = Max(0d, cdf);
+
+                    return cdf;
+                }
+                else {
+                    ddouble cdf = -Expm1(-u);
+
+                    return cdf;
+                }
+            }
+            else {
+                if (IsNegative(u)) {
+                    return 1d;
+                }
+                if (IsPositiveInfinity(u)) {
+                    return 0d;
+                }
+
+                if (IsFinite(cdf_power)) {
+                    ddouble cdf = Pow(Max(0d, 1d + (Q - 1d) * u), cdf_power);
+
+                    cdf = Min(1d, cdf);
+
+                    return cdf;
+                }
+                else {
+                    ddouble cdf = Exp(-u);
+
+                    return cdf;
+                }
+            }
+        }
+
+        public override ddouble Quantile(ddouble p, Interval interval = Interval.Lower) {
+            if (!InRangeUnit(p)) {
+                return NaN;
+            }
+
+            ddouble u;
+
+            if (interval == Interval.Lower) {
+                if (p >= 1d) {
+                    return Support.max;
+                }
+
+                if (Q != 1d) {
+                    u = (Pow(1d - p, quantile_power) - 1d) / (Q - 1d);
+                }
+                else {
+                    u = -Log1p(-p);
+                }
+            }
+            else {
+                if (p <= 0d) {
+                    return Support.max;
+                }
+                
+                if (Q != 1d) {
+                    u = (Pow(p, quantile_power) - 1d) / (Q - 1d);
+                }
+                else {
+                    u = -Log(p);
+                }
+            }
+
+            if (IsNegative(u)) {
+                return 0d;
+            }
+
+            ddouble x = u * Theta;
+            return x;
+        }
+
+        public override double Sample(Random random) {
+            double u = random.NextUniformOpenInterval01();
+
+            double r = (Q != 1d)
+                ? (double.Pow(u, (double)quantile_power) - 1d) / ((double)Q - 1d)
+                : -double.Log(u);
+
+            double w = r * (double)Theta;
+
+            return w;
+        }
+
+        public override (ddouble min, ddouble max) Support => Q < 1d
+            ? (0d, Theta / (1d - Q))
+            : (0d, PositiveInfinity);
+
+        public override ddouble Mean => (Q < 1.5d)
+            ? Theta / (3d - 2d * Q)
+            : NaN;
+
+        public override ddouble Median => Quantile(0.5d);
+
+        public override ddouble Mode => 0d;
+
+        public override ddouble Variance => (Q * 3d < 4d)
+            ? Theta * Theta * (Q - 2d) / (Square(2d * Q - 3d) * (3d * Q - 4d))
+            : NaN;
+
+        public override ddouble Skewness => (Q < 1.25d)
+            ? 2d / (5d - 4d * Q) * Sqrt((3d * Q - 4d) / (Q - 2d))
+            : NaN;
+
+        public override ddouble Kurtosis => (Q * 5d < 6d)
+            ? 6d * (6d + Q * (-20d + Q * (17d + Q * -4d))) / ((Q - 2d) * (4d * Q - 5d) * (5d * Q - 6d))
+            : NaN;
+
+        private ddouble? entropy = null;
+        public override ddouble Entropy => entropy ??=
+            IntegrationStatistics.Entropy(this, eps: 1e-28, discontinue_eval_points: 32768);
+
+        public static QExponentialDistribution operator *(QExponentialDistribution dist, ddouble k) {
+            return new(dist.Q, dist.Theta * k);
+        }
+
+        public static QExponentialDistribution operator /(QExponentialDistribution dist, ddouble k) {
+            return new(dist.Q, dist.Theta / k);
+        }
+
+        public static (QExponentialDistribution? dist, ddouble error) Fit(IEnumerable<double> samples, (double min, double max) fitting_quantile_range, int quantile_partitions = 100)
+            => Fit(samples.Select(v => (ddouble)v), fitting_quantile_range, quantile_partitions);
+
+        public static (QExponentialDistribution? dist, ddouble error) Fit(IEnumerable<ddouble> samples, (ddouble min, ddouble max) fitting_quantile_range, int quantile_partitions = 100) {
+            ddouble[] qs = EnumerableUtil.Linspace(fitting_quantile_range.min, fitting_quantile_range.max, quantile_partitions + 1, end_point: true).ToArray();
+            ddouble[] ys = samples.Quantile(qs).ToArray();
+
+            ddouble q = BisectionMinimizeSearch1D.Search(
+                q => {
+                    try {
+                        QExponentialDistribution dist = new(q, 1d);
+                        return QuantileScaleFitter<QExponentialDistribution>.FitForQuantiles(dist, qs, ys).error;
+                    }
+                    catch (ArgumentOutOfRangeException) {
+                        return NaN;
+                    }
+
+                }, (0, 1.9995d), iter: 32
+            );
+
+            try {
+                QExponentialDistribution dist = new(q, 1d);
+
+                return QuantileScaleFitter<QExponentialDistribution>.FitForQuantiles(dist, qs, ys);
+            }
+            catch (ArgumentOutOfRangeException) {
+                return (null, NaN);
+            }
+        }
+
+        public override string ToString() {
+            return $"{typeof(QExponentialDistribution).Name}[q={Q},theta={Theta}]";
+        }
+
+        public override string Formula => "p(x; q, theta) := (2d - Q) * e_q(-u) / theta, u = x / theta";
+    }
+}
---
+++ DoubleDoubleStatisticTest/ContinuousDistributions/ScalableDistribution/QExponentialDistributionTests.cs
@@ -0,0 +1,394 @@
+﻿using DoubleDouble;
+using DoubleDoubleStatistic;
+using DoubleDoubleStatistic.ContinuousDistributions;
+using DoubleDoubleStatistic.SampleStatistic;
+using DoubleDoubleStatistic.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DoubleDoubleStatisticTest.ContinuousDistributions.ScalableDistribution {
+    [TestClass()]
+    public class QExponentialDistributionTests {
+        readonly QExponentialDistribution dist_q025theta1 = new(q: 0.25, theta: 1);
+        readonly QExponentialDistribution dist_q050theta2 = new(q: 0.50, theta: 2);
+        readonly QExponentialDistribution dist_q075theta2 = new(q: 0.75, theta: 2);
+        readonly QExponentialDistribution dist_q100theta3 = new(q: 1.00, theta: 3);
+        readonly QExponentialDistribution dist_q125theta6 = new(q: 1.25, theta: 6);
+        readonly QExponentialDistribution dist_q150theta1 = new(q: 1.50, theta: 1);
+        readonly QExponentialDistribution dist_q175theta4 = new(q: 1.75, theta: 4);
+
+        QExponentialDistribution[] Dists => [
+            dist_q025theta1,
+            dist_q050theta2,
+            dist_q075theta2,
+            dist_q100theta3,
+            dist_q125theta6,
+            dist_q150theta1,
+            dist_q175theta4,
+        ];
+
+        [TestMethod()]
+        public void InfoTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                Console.WriteLine($"Support={dist.Support}");
+                Console.WriteLine($"Q={dist.Q}");
+                Console.WriteLine($"Theta={dist.Theta}");
+                Console.WriteLine($"Mean={dist.Mean}");
+                Console.WriteLine($"Median={dist.Median}");
+                Console.WriteLine($"Mode={dist.Mode}");
+                Console.WriteLine($"Variance={dist.Variance}");
+                Console.WriteLine($"Skewness={dist.Skewness}");
+                Console.WriteLine($"Kurtosis={dist.Kurtosis}");
+                Console.WriteLine($"Entropy={dist.Entropy}");
+                Console.WriteLine(dist.Formula);
+            }
+        }
+
+        [TestMethod()]
+        public void MeanTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+
+                if (ddouble.IsNaN(dist.Mean)) {
+                    continue;
+                }
+
+                ddouble actual = dist.Mean;
+                ddouble expected = IntegrationStatistics.Mean(dist, eps: 1e-28, discontinue_eval_points: 65536);
+                Assert.IsTrue(ddouble.Abs(actual - expected) < 1e-28, $"{dist}\n{expected}\n{actual}");
+            }
+        }
+
+        [TestMethod()]
+        public void ModeTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+
+                if (ddouble.IsNaN(dist.Mode)) {
+                    continue;
+                }
+
+                Assert.IsTrue(dist.PDF(dist.Mode) > dist.PDF(dist.Mode - 1e-4), $"{dist}\n{dist.Mode}");
+                Assert.IsTrue(dist.PDF(dist.Mode) > dist.PDF(dist.Mode + 1e-4), $"{dist}\n{dist.Mode}");
+            }
+        }
+
+        [TestMethod()]
+        public void MedianTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+
+                Assert.IsTrue(ddouble.Abs(dist.CDF(dist.Median) - 0.5) < 1e-20, $"{dist}\n{dist.Median}");
+            }
+        }
+
+        [TestMethod()]
+        public void VarianceTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+
+                if (!ddouble.IsFinite(dist.Variance)) {
+                    continue;
+                }
+
+                ddouble actual = dist.Variance;
+                ddouble expected = IntegrationStatistics.Variance(dist, eps: 1e-28, discontinue_eval_points: 65536);
+                Assert.IsTrue(ddouble.Abs(actual - expected) < 1e-20, $"{dist}\n{expected}\n{actual}");
+            }
+        }
+
+        [TestMethod()]
+        public void SkewnessTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+
+                if (!ddouble.IsFinite(dist.Skewness)) {
+                    continue;
+                }
+
+                ddouble actual = dist.Skewness;
+                ddouble expected = IntegrationStatistics.Skewness(dist, eps: 1e-28, discontinue_eval_points: 65536);
+                Assert.IsTrue(ddouble.Abs(actual - expected) < 1e-20, $"{dist}\n{expected}\n{actual}");
+            }
+        }
+
+        [TestMethod()]
+        public void KurtosisTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+
+                if (!ddouble.IsFinite(dist.Kurtosis)) {
+                    continue;
+                }
+
+                ddouble actual = dist.Kurtosis;
+                ddouble expected = IntegrationStatistics.Kurtosis(dist, eps: 1e-28, discontinue_eval_points: 65536);
+                Assert.IsTrue(ddouble.Abs(actual - expected) < 1e-20, $"{dist}\n{expected}\n{actual}");
+            }
+        }
+
+        [TestMethod()]
+        public void EntropyTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+
+                ddouble actual = dist.Entropy;
+                ddouble expected = IntegrationStatistics.Entropy(dist, eps: 1e-28, discontinue_eval_points: 65536);
+                Assert.IsTrue(ddouble.Abs(actual - expected) < 1e-20, $"{dist}\n{expected}\n{actual}");
+            }
+        }
+
+        [TestMethod()]
+        public void PDFTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                for (ddouble x = -1; x <= 5; x += 0.125) {
+                    ddouble pdf = dist.PDF(x);
+
+                    Console.WriteLine($"pdf({x})={pdf}");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void CDFLowerTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                for (ddouble x = -1; x <= 5; x += 0.125) {
+                    ddouble cdf = dist.CDF(x, Interval.Lower);
+
+                    Console.WriteLine($"cdf({x})={cdf}");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void CDFUpperTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                for (ddouble x = -1; x <= 5; x += 0.125) {
+                    ddouble cdf = dist.CDF(x, Interval.Lower);
+                    ddouble ccdf = dist.CDF(x, Interval.Upper);
+
+                    Console.WriteLine($"ccdf({x})={ccdf}");
+
+                    Assert.IsTrue(ddouble.Abs(cdf + ccdf - 1) < 1e-28);
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void QuantileLowerTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                for (int i = 0; i <= 10; i++) {
+                    ddouble p = (ddouble)i / 10;
+                    ddouble x = dist.Quantile(p, Interval.Lower);
+                    ddouble cdf = dist.CDF(x, Interval.Lower);
+
+                    Console.WriteLine($"quantile({p})={x}, cdf({x})={cdf}");
+
+                    if (ddouble.IsFinite(x)) {
+                        Assert.IsTrue(ddouble.Abs(p - cdf) < 1e-28);
+                    }
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void QuantileUpperTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+                for (int i = 0; i <= 10; i++) {
+                    ddouble p = (ddouble)i / 10;
+                    ddouble x = dist.Quantile(p, Interval.Upper);
+                    ddouble ccdf = dist.CDF(x, Interval.Upper);
+
+                    Console.WriteLine($"cquantile({p})={x}, ccdf({x})={ccdf}");
+
+                    if (ddouble.IsFinite(x)) {
+                        Assert.IsTrue(ddouble.Abs(p - ccdf) < 1e-28);
+                    }
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void RandomGenerateTest() {
+            Random random = new(1234);
+
+            foreach (QExponentialDistribution dist in Dists) {
+                // ignore
+                if (dist.Q > 1.5) {
+                    continue;
+                }
+
+                Console.WriteLine(dist);
+
+                double[] xs = dist.Sample(random, 100000).ToArray();
+
+                double max_error = 0d;
+
+                for (int i = 5; i <= 90; i++) {
+                    double p = (double)i / 100;
+                    double expected = (double)dist.Quantile(p, Interval.Lower);
+                    double actual = xs.Quantile((double)p);
+
+                    max_error = double.Max(max_error, double.Abs(expected - actual));
+
+                    Assert.AreEqual(expected, actual, (double.Abs(expected) + 1) * 0.04, $"{p}\n{expected}\n{actual}");
+                }
+
+                Console.WriteLine(max_error);
+            }
+        }
+
+        [TestMethod()]
+        public void FitTest() {
+            Random random = new(1234);
+
+            foreach (QExponentialDistribution dist in Dists) {
+                // ignore
+                if (dist.Q > 1.5) {
+                    continue;
+                }
+
+                Console.WriteLine(dist);
+
+                double[] xs = dist.Sample(random, 100000).ToArray();
+
+                (QExponentialDistribution? dist_fit, ddouble error) = QExponentialDistribution.Fit(xs, (0.05, 0.95));
+
+                Assert.IsNotNull(dist_fit);
+
+                Console.WriteLine(dist_fit);
+                Console.WriteLine(error);
+
+                Assert.IsTrue(error < 5e-2);
+            }
+        }
+
+        [TestMethod()]
+        public void IrregularValueTest() {
+            foreach (QExponentialDistribution dist in Dists) {
+                Console.WriteLine(dist);
+
+                Assert.IsTrue(ddouble.IsFinite(dist.PDF(ddouble.NegativeInfinity)) && dist.PDF(ddouble.NegativeInfinity) >= 0d, "pdf(-inf)");
+                Assert.IsTrue(ddouble.IsFinite(dist.PDF(ddouble.MinValue)) && dist.PDF(ddouble.MinValue) >= 0d, "pdf(-lval)");
+                Assert.IsTrue(ddouble.IsFinite(dist.PDF(ddouble.MinValue / 2)) && dist.PDF(ddouble.MinValue / 2) >= 0d, "pdf(-lval / 2)");
+
+                Assert.IsTrue(ddouble.IsFinite(dist.PDF(ddouble.PositiveInfinity)) && dist.PDF(ddouble.PositiveInfinity) >= 0d, "pdf(+inf)");
+                Assert.IsTrue(ddouble.IsFinite(dist.PDF(ddouble.MaxValue)) && dist.PDF(ddouble.MaxValue) >= 0d, "pdf(+lval)");
+                Assert.IsTrue(ddouble.IsFinite(dist.PDF(ddouble.MaxValue / 2)) && dist.PDF(ddouble.MaxValue / 2) >= 0d, "pdf(+lval / 2)");
+
+                Assert.IsTrue(ddouble.IsNaN(dist.PDF(ddouble.NaN)), "pdf(NaN)");
+
+                Assert.IsTrue(dist.CDF(ddouble.NegativeInfinity, Interval.Lower) == 0d, "cdf(-inf)");
+                Assert.IsTrue(ddouble.IsFinite(dist.CDF(ddouble.MinValue, Interval.Lower)) && dist.CDF(ddouble.MinValue, Interval.Lower) >= 0d, "cdf(-lval)");
+                Assert.IsTrue(ddouble.IsFinite(dist.CDF(ddouble.MinValue / 2, Interval.Lower)) && dist.CDF(ddouble.MinValue / 2, Interval.Lower) >= 0d, "cdf(-lval / 2)");
+
+                Assert.IsTrue(dist.CDF(ddouble.PositiveInfinity, Interval.Lower) == 1d, "cdf(+inf)");
+                Assert.IsTrue(ddouble.IsFinite(dist.CDF(ddouble.MaxValue, Interval.Lower)) && dist.CDF(ddouble.MaxValue, Interval.Lower) <= 1d, "cdf(+lval)");
+                Assert.IsTrue(ddouble.IsFinite(dist.CDF(ddouble.MaxValue / 2, Interval.Lower)) && dist.CDF(ddouble.MaxValue / 2, Interval.Lower) <= 1d, "cdf(+lval / 2)");
+
+                Assert.IsTrue(ddouble.IsNaN(dist.CDF(ddouble.NaN, Interval.Lower)), "cdf(NaN)");
+
+                Assert.IsTrue(dist.CDF(ddouble.NegativeInfinity, Interval.Upper) == 1d, "ccdf(-inf)");
+                Assert.IsTrue(ddouble.IsFinite(dist.CDF(ddouble.MinValue, Interval.Upper)) && dist.CDF(ddouble.MinValue, Interval.Upper) <= 1d, "ccdf(-lval)");
+                Assert.IsTrue(ddouble.IsFinite(dist.CDF(ddouble.MinValue / 2, Interval.Upper)) && dist.CDF(ddouble.MinValue / 2, Interval.Upper) <= 1d, "ccdf(-lval / 2)");
+
+                Assert.IsTrue(dist.CDF(ddouble.PositiveInfinity, Interval.Upper) == 0d, "cdf(+inf)");
+                Assert.IsTrue(ddouble.IsFinite(dist.CDF(ddouble.MaxValue, Interval.Upper)) && dist.CDF(ddouble.MaxValue, Interval.Upper) >= 0d, "ccdf(+lval)");
+                Assert.IsTrue(ddouble.IsFinite(dist.CDF(ddouble.MaxValue / 2, Interval.Upper)) && dist.CDF(ddouble.MaxValue / 2, Interval.Upper) >= 0d, "ccdf(+lval / 2)");
+
+                Assert.IsTrue(ddouble.IsNaN(dist.CDF(ddouble.NaN, Interval.Upper)), "ccdf(NaN)");
+
+                Assert.IsTrue(ddouble.IsFinite(dist.Quantile(0d, Interval.Lower)) || ddouble.IsNegativeInfinity(dist.Quantile(0d, Interval.Lower)), "quantile(0)");
+                Assert.IsTrue(ddouble.IsFinite(dist.Quantile(1d, Interval.Lower)) || ddouble.IsPositiveInfinity(dist.Quantile(1d, Interval.Lower)), "quantile(1)");
+
+                Assert.IsTrue(ddouble.IsFinite(dist.Quantile(0d, Interval.Upper)) || ddouble.IsPositiveInfinity(dist.Quantile(0d, Interval.Upper)), "cquantile(0)");
+                Assert.IsTrue(ddouble.IsFinite(dist.Quantile(1d, Interval.Upper)) || ddouble.IsNegativeInfinity(dist.Quantile(1d, Interval.Upper)), "cquantile(1)");
+
+                Assert.IsTrue(ddouble.IsFinite(dist.Quantile(ddouble.Epsilon, Interval.Lower)) || ddouble.IsNegativeInfinity(dist.Quantile(ddouble.Epsilon, Interval.Lower)), "quantile(0+eps)");
+                Assert.IsTrue(ddouble.IsFinite(dist.Quantile(ddouble.One - ddouble.Epsilon, Interval.Lower)) || ddouble.IsPositiveInfinity(dist.Quantile(ddouble.One - ddouble.Epsilon, Interval.Lower)), "quantile(1-eps)");
+
+                Assert.IsTrue(ddouble.IsFinite(dist.Quantile(ddouble.Epsilon, Interval.Upper)) || ddouble.IsPositiveInfinity(dist.Quantile(ddouble.Epsilon, Interval.Upper)), "cquantile(0+eps)");
+                Assert.IsTrue(ddouble.IsFinite(dist.Quantile(ddouble.One - ddouble.Epsilon, Interval.Upper)) || ddouble.IsNegativeInfinity(dist.Quantile(ddouble.One - ddouble.Epsilon, Interval.Upper)), "cquantile(1-eps)");
+            }
+        }
+
+        [TestMethod()]
+        public void PDFExpectedTest() {
+            ddouble[] expected_dist_q025theta1 = [];
+            ddouble[] expected_dist_q050theta2 = [];
+            ddouble[] expected_dist_q075theta2 = [];
+            ddouble[] expected_dist_q100theta3 = [];
+            ddouble[] expected_dist_q125theta6 = [];
+            ddouble[] expected_dist_q150theta1 = [];
+            ddouble[] expected_dist_q175theta4 = [];
+
+            foreach ((QExponentialDistribution dist, ddouble[] expecteds) in new[]{
+                (dist_q025theta1, expected_dist_q025theta1),
+                (dist_q050theta2, expected_dist_q050theta2),
+                (dist_q075theta2, expected_dist_q075theta2),
+                (dist_q100theta3, expected_dist_q100theta3),
+                (dist_q125theta6, expected_dist_q125theta6),
+                (dist_q150theta1, expected_dist_q150theta1),
+                (dist_q175theta4, expected_dist_q175theta4),
+            }) {
+                for ((ddouble x, int i) = (0, 0); i < expecteds.Length; x += 0.25, i++) {
+                    ddouble expected = expecteds[i];
+                    ddouble actual = dist.PDF(x);
+
+                    Console.WriteLine($"{dist} pdf({x})");
+                    Console.WriteLine(expected);
+                    Console.WriteLine(actual);
+
+                    if (expected > 0) {
+                        Assert.IsTrue(ddouble.Abs(expected - actual) / expected < 1e-10, $"{dist} pdf({x})\n{expected}\n{actual}");
+                    }
+                    else {
+                        Assert.AreEqual(0, actual);
+                    }
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void CDFExpectedTest() {
+            ddouble[] expected_dist_q025theta1 = [];
+            ddouble[] expected_dist_q050theta2 = [];
+            ddouble[] expected_dist_q075theta2 = [];
+            ddouble[] expected_dist_q100theta3 = [];
+            ddouble[] expected_dist_q125theta6 = [];
+            ddouble[] expected_dist_q150theta1 = [];
+            ddouble[] expected_dist_q175theta4 = [];
+
+            foreach ((QExponentialDistribution dist, ddouble[] expecteds) in new[]{
+                (dist_q025theta1, expected_dist_q025theta1),
+                (dist_q050theta2, expected_dist_q050theta2),
+                (dist_q075theta2, expected_dist_q075theta2),
+                (dist_q100theta3, expected_dist_q100theta3),
+                (dist_q125theta6, expected_dist_q125theta6),
+                (dist_q150theta1, expected_dist_q150theta1),
+                (dist_q175theta4, expected_dist_q175theta4),
+            }) {
+                for ((ddouble x, int i) = (0, 0); i < expecteds.Length; x += 0.25, i++) {
+                    ddouble expected = expecteds[i];
+                    ddouble actual = dist.CDF(x);
+
+                    Console.WriteLine($"{dist} cdf({x})");
+                    Console.WriteLine(expected);
+                    Console.WriteLine(actual);
+
+                    if (expected > 0) {
+                        Assert.IsTrue(ddouble.Abs(expected - actual) / expected < 1e-10, $"{dist} cdf({x})\n{expected}\n{actual}");
+                    }
+                    else {
+                        Assert.AreEqual(0, actual);
+                    }
+                }
+            }
+        }
+    }
+}
